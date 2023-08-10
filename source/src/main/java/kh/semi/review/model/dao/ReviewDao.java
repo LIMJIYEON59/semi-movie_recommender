64e47258f90d8c08145546c880ded9318e835864
LIMJIYEON59 <chayi2110@gmail.com>
2023-08-10 18:44:24 +0900

    리뷰,좋아요 Dao 만듬
    1.리뷰 insert
    2.좋아요 insert,check,delete
    3.ReviewVo 리뷰와 좋아요 위치 바꿈
    4.좋아요 이미지 저장

--- source/src/main/java/kh/semi/review/model/dao/ReviewDao.java
+++ source/src/main/java/kh/semi/review/model/dao/ReviewDao.java
@@ -14,8 +14,9 @@ public class ReviewDao {
 		
 //		INSERT INTO REVIEW (REVIEW_ID, MOVIE_ID, EMAIL, CONTENT)
 //		VALUES (1, 200101, 'ABC@NAVER.COM', '정말재밌어요');
-		String query="INSERT INTO REVIEW (REVIEW_ID, MOVIE_ID, EMAIL, CONTENT=?";
-		query += "VALUES (?, ?, ?, ?)";
+//		INSERT INTO LIKED (REVIEW_ID, EMAIL)
+//		VALUES (1, 'ABC@NAVER.COM');
+		String query="INSERT INTO REVIEW (REVIEW_ID, MOVIE_ID, EMAIL, CONTENT) VALUES (?, ?, ?, ?)";
 		
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
@@ -25,8 +26,8 @@ public class ReviewDao {
 			pstmt.setInt(1, vo.getReviewId());
 			pstmt.setInt(2, vo.getMovieId());
 			pstmt.setString(3, vo.getEmail());
-			pstmt.setString(3, vo.getContent());
-			result = pstmt.executeUpdate();
+			pstmt.setString(4, vo.getContent());
+			result = pstmt.executeUpdate();	//리뷰 등록 실행
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {
@@ -34,10 +35,108 @@ public class ReviewDao {
 			JdbcTemplate.close(pstmt);
 		}
 			
-		return result;
+		return result;	// 리뷰 등록 결과 반환
+	}
+	// 좋아요 등록
+	public int insertLinked(Connection conn, ReviewVo vo) {
+		int result = 0;
+
+		String query="INSERT INTO LIKED (REVIEW_ID, EMAIL) VALUES (?, ?)";
+		
+		PreparedStatement pstmt = null;
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, vo.getReviewId());
+			pstmt.setString(2, vo.getEmail());
+			result = pstmt.executeUpdate();	// 좋아요 추가 실행함!
+			if(result > 0){
+				System.out.println("좋아요가 추가 되었습니다.");
+			}else{
+				System.out.println("좋아요가 추가에 실패했습니다.");
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			JdbcTemplate.close(pstmt);
+		}
+			
+		return result;	// 좋아요 추가 결과 반환
 	}
 
-	//리뷰 조회
+		// 좋아요 취소
+		public int deleteLiked(Connection conn, int review_Id, String email) {
+			int result = 0;
+			
+			String query ="DELETE FROM LIKED WHERE REVIEW_ID =? AND EMAIL =?";
+			
+			PreparedStatement pstmt = null;
+		
+			try {
+				pstmt = conn.prepareStatement(query);
+				pstmt.setInt(1,review_Id);
+				pstmt.setString(2, email);
+				result = pstmt.executeUpdate();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				JdbcTemplate.close(pstmt);
+			}
 	
+			return result;
+		}
+		
+//		// 좋아요 수 카운트
+//		public int updateLikeCount(Connection conn, String review_Id, String email) {			
+//			int result = 0;
+//			
+//			String query = "UPDATE COUNTE(*) FROM LIKED =?";
+//			
+//			PreparedStatement pstmt = null;
+//			
+//			try {
+//				pstmt = conn.prepareStatement(query);
+//				pstmt.setInt(1, Integer.parseInt(review_Id));
+//				result = pstmt.executeUpdate();
+//			} catch (SQLException e) {
+//				e.printStackTrace();
+//			}finally {
+//				JdbcTemplate.close(pstmt);
+//			}
+//			
+//			return result;
+//		
+//			
+//		}
+		
+		//좋아요 확인  
+		public boolean checkLiked(Connection conn, int review_Id, String email) {
+			boolean rliked = false;
+			
+			String query="SELECT COUNT(*) FROM LIKED WHERE REVIEW_ID=? AND EMAIL=?";
+			
+			PreparedStatement pstmt = null;
+			ResultSet rs = null;
+			
+			try {
+				pstmt = conn.prepareStatement(query);
+				pstmt.setInt(1, review_Id);
+				pstmt.setString(2, email);
+				rs = pstmt.executeQuery();
+				if(rs.next()) {
+					int count = rs.getInt(1);	//가져 온 좋아요를 count에 저장한다.
+					 rliked = count > 0;
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}finally {
+				JdbcTemplate.close(rs);
+				JdbcTemplate.close(pstmt);
+			}
+				
+			return rliked;
+		}
+		
 	
 }
--- source/src/main/java/kh/semi/review/model/vo/ReviewVo.java
+++ source/src/main/java/kh/semi/review/model/vo/ReviewVo.java
@@ -6,37 +6,39 @@ public class ReviewVo {
 //	"EMAIL"	VARCHAR2(50)		NOT NULL,
 //	"CONTENT"	VARCHAR2(200)		NOT NULL
 	
-	//<좋아요>
-	//여기다가 select문을 해서 넣어준다.
-	private boolean myLiked;	//내가 누른 것
-	private int cntLiked;	    //좋아요 수 카운트
-	
 	private int reviewId;
 	private int movieId;
 	private String email;
 	private String content;
 	
+	//<좋아요>
+	//여기다가 select문을 해서 넣어준다.
+	private boolean myLiked;	//내가 누른 것
+	private int cntLiked;	    //좋아요 수 카운트
+	
 	public ReviewVo() {
 		super();
 	}
 	
-	
-	public ReviewVo(boolean myLiked, int cntLiked, int reviewId, int movieId, String email, String content) {
+
+	public ReviewVo(int reviewId, int movieId, String email, String content, boolean myLiked, int cntLiked) {
 		super();
-		this.myLiked = myLiked;
-		this.cntLiked = cntLiked;
 		this.reviewId = reviewId;
 		this.movieId = movieId;
 		this.email = email;
 		this.content = content;
+		this.myLiked = myLiked;
+		this.cntLiked = cntLiked;
 	}
 
 
 	@Override
 	public String toString() {
-		return "ReviewVo [myLiked=" + myLiked + ", cntLiked=" + cntLiked + ", reviewId=" + reviewId + ", movieId="
-				+ movieId + ", email=" + email + ", content=" + content + "]";
+		return "ReviewVo [reviewId=" + reviewId + ", movieId=" + movieId + ", email=" + email + ", content=" + content
+				+ ", myLiked=" + myLiked + ", cntLiked=" + cntLiked + "]";
 	}
+
+
 	public boolean isMyLiked() {
 		return myLiked;
 	}
